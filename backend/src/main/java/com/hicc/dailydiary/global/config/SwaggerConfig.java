dcb7061c57e9cc556bc74ecaf98d9723ae01b32a
Jeongyeop Lyou <amused170@gmail.com>
2026-07-31 14:37:59 +0900

    setting: H2 DB 및 스웨거 연결 설정

--- backend/src/main/java/com/hicc/dailydiary/global/config/SwaggerConfig.java
+++ backend/src/main/java/com/hicc/dailydiary/global/config/SwaggerConfig.java
@@ -1,4 +1,21 @@
 package com.hicc.dailydiary.global.config;
 
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI openAPI() {
+        Info info = new Info()
+                .title("하루한줄 (Daily Diary) API 문서")
+                .version("v1.0.0")
+                .description("하루한줄의 백엔드 API 명세서입니다.");
+
+        return new OpenAPI()
+                .info(info);
+    }
 }
